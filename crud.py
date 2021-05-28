a87248c9f6b79c8f2dd30fdaa498cf0f9e9b4af9
Jenny <han.jennyjh@outlook.com>
2021-05-28 23:32:34 +0000

    Make get random painting function
    Co-authored-by: Munawaer Ainiwaer <munawaer.ainiwaer@gmail.com>

--- crud.py
+++ crud.py
@@ -1,6 +1,7 @@
 """Create, read, update and delete"""
 
 from random import choice
+import csv
 
 def get_random_quote():
     """Open text file and get a random quote"""
@@ -12,6 +13,13 @@ def get_random_quote():
 
         return quote
 
+def get_random_painting():
+    """Get a random painting from the csv file."""
 
+    f = open('./Bob_Ross_Paintings/data/bob_ross_paintings.csv')
+    paintings_f = csv.reader(f)
     
+    for row in paintings_f:
+        print(row)
+        break
 
