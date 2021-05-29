5ad1602c5e815fb4e94f3ca89ad9580066e69710
Jenny <han.jennyjh@outlook.com>
2021-05-29 23:22:19 +0000

    All features made
    Co-authored-by: Munawaer Ainiwaer <munawaer.ainiwaer@gmail.com>

--- static/javascript/bob.js
+++ static/javascript/bob.js
@@ -15,7 +15,7 @@ $(window).on('load', ()=>{
       $('#new_image').html('<img id="currentImage" src='+res.new_paint+'>');
       $('#colorContain').empty();
       for (item of res.new_colors){
-        $('#colorContain').append('<div class="col" style="background-color:'+item+'">color</div>');      
+        $('#colorContain').append('<div class="col" style="background-color:'+item+';color:'+item+'">color</div>');      
       };
       $('input#paintingURL').val(res.new_paint)
       $('input#youtubeURL').val(res.vid_link)
@@ -27,5 +27,33 @@ $(window).on('load', ()=>{
 
 });
 
+function _(selector){
+  return document.querySelector(selector);
+}
+function setup(){
+  let canvas = createCanvas(650, 600);
+  canvas.parent("canvas-wrapper");
+  background(255);
+}
+function mouseDragged(){
+  let type = _("#pen-pencil").checked?"pencil":"brush";
+  let size = parseInt(_("#pen-size").value);
+  let color =  _("#pen-color").value;
+  fill(color);
+  stroke(color);
+  if(type == "pencil"){
+line(pmouseX, pmouseY, mouseX, mouseY);
+  } else {
+    ellipse(mouseX, mouseY, size, size)
+  }
+}
+
+_("#reset-canvas").addEventListener("click",function(){
+  background(255);
+});
+
+_("#save-canvas").addEventListener("click", function(){
+  saveCanvas(canvas, "sketch", "png");
+});
 
 
