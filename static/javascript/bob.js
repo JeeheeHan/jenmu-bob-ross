f22f3de4784a23a6f4b193d3458549947576ebce
Jenny <han.jennyjh@outlook.com>
2021-05-30 00:52:58 +0000

    placed quotes
    Co-authored-by: Munawaer Ainiwaer <munawaer.ainiwaer@gmail.com>

--- static/javascript/bob.js
+++ static/javascript/bob.js
@@ -12,7 +12,10 @@ $(window).on('load', ()=>{
     morequotes.done((res) => {
       //Input quote, img and colors into page per button click
       $('#currentQuote').html(res.quote);
-      $('#new_image').html('<img id="currentImage" src='+res.new_paint+'>');
+      // $('#new_image').html('<img id="currentImage" src='+res.new_paint+'>');
+      //background image
+      $('#background-image').html('<img id="currentImage" src='+res.new_paint+'>');
+
       $('#colorContain').empty();
       for (item of res.new_colors){
         $('#colorContain').append('<div class="col" style="background-color:'+item+';color:'+item+'">color</div>');      
@@ -23,37 +26,8 @@ $(window).on('load', ()=>{
     });
   });
 
-  
 
 });
 
-function _(selector){
-  return document.querySelector(selector);
-}
-function setup(){
-  let canvas = createCanvas(650, 600);
-  canvas.parent("canvas-wrapper");
-  background(255);
-}
-function mouseDragged(){
-  let type = _("#pen-pencil").checked?"pencil":"brush";
-  let size = parseInt(_("#pen-size").value);
-  let color =  _("#pen-color").value;
-  fill(color);
-  stroke(color);
-  if(type == "pencil"){
-line(pmouseX, pmouseY, mouseX, mouseY);
-  } else {
-    ellipse(mouseX, mouseY, size, size)
-  }
-}
-
-_("#reset-canvas").addEventListener("click",function(){
-  background(255);
-});
-
-_("#save-canvas").addEventListener("click", function(){
-  saveCanvas(canvas, "sketch", "png");
-});
 
 
